c889e4c1842c9c1b0dbe6d648ba4d1e4b2d80b96
Student User <student@workstation.lab.example.com>
2021-02-24 03:55:05 -0500

    Materials for lab pg80.

--- DO288/labs/design-container/oc-new-app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source /usr/local/etc/ocp4.config
-
-oc new-app  --as-deployment-config --name elvis \
-    "https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps#design-container" \
-    --context-dir hello-java
